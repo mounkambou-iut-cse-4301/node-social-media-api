f15e0b537aba6e87db5039256412b6a01561d41e
abdelkarim <abdelkarim@iut-dhaka.edu>
2023-08-11 15:23:15 +0100

    Creating Post Model

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv=require("dotenv");
 const helmet=require("helmet");
 const morgan=require("morgan");
 const userRoute=require('./routes/users')
+const postRoute=require('./routes/posts')
 const authRoute=require('./routes/auth')
 dotenv.config();
 const port=process.env.PORT
@@ -22,6 +23,7 @@ app.use(morgan("common"));
 
 app.use("/api/users",userRoute)
 app.use("/api/auth",authRoute)
+app.use("/api/posts",postRoute)
 app.listen(port,()=>{
     console.log(`Backend server is running on ${port}`);
 })
